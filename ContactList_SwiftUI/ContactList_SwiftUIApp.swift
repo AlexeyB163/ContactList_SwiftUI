f09b9b3c8f7d9fc2efc1f2614d0c3995af273329
AlexeyB163 <sourceaisa@mail.ru>
2022-04-14 11:08:00 +0400

    Initial Commit

---
+++ ContactList_SwiftUI/ContactList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUIApp.swift
+//  ContactList_SwiftUI
+//
+//  Created by User on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
